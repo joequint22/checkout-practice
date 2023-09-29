9d20d3e8cbf7dcabd3d041f72add258f3cb2ad5a
joequint22 <joequintiliano4wk@gmail.com>
2023-09-29 16:31:57 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [itemsInPersonCart, setItemsInPersonCart] = useState(0)
+  const [lines, setLines] = useState([ [], [], [], [], []])
+    
+function addPersonToLine(e){
+  e.preventDefault();
+  let leastItemAmount = 1e9;
+  let lineWithLeast = null;
+
+
+  for(let line of lines){
+    const totalItemAmount = line.reduce((sum, value) => sum + value, 0)
+    if(totalItemAmount < leastItemAmount){
+        leastItemAmount = totalItemAmount
+        lineWithLeast = line
+    }
+  }
+  
+  setLines(prevLines => (
+    prevLines.map((line, idx) => (
+     line === lineWithLeast ? [...line, itemsInPersonCart] : line
+    ))
+  ))
+}
+
+ useEffect(() => {
+    const interval = setInterval(() => {
+      setLines(prevLines => (
+        prevLines.map((line) => (
+          [line[0] - 1, ...line.slice(1)].filter((value) => value > 0)
+        ))
+      ))
+
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  })
+
+
+return (
+  <div className="App">
+    <form onSubmit={addPersonToLine}className="">
+      <input 
+        type="number"
+        required
+        onChange={e => setItemsInPersonCart(e.currentTarget.valueAsNumber)}
+        value= {itemsInPersonCart} />
+        <button>Checkout</button>
+    </form>
+
+
+    <div className="lines">
+        {lines.map((line, idx) => {
+         return  <div key={idx} className="line">
+            {line.map((numberOfItems, lineIdx) => {
+              return <div key={lineIdx}>{numberOfItems}</div>
+            })}
+          </div>
+        })}
     </div>
+  </div>
   );
 }
 
